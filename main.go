67e3b0856620185a19c4b30a995663c0b2aff116
Senthil Arivudainambi <senthil@walkerandcobrands.com>
2018-01-17 20:43:30 -0800

    copy over example Go lambda; update Makefile

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+import (
+	"errors"
+	"log"
 
-func Handle(evt interface{}, ctx *runtime.Context) (string, error) {
-	return "Hello, World!", nil
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var (
+	// ErrNameNotProvided is thrown when a name is not provided
+	ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
+)
+
+// Handler is your Lambda function handler
+// It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
+// However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// stdout and stderr are sent to AWS CloudWatch Logs
+	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
+
+	// If no name is provided in the HTTP request body, throw an error
+	if len(request.Body) < 1 {
+		return events.APIGatewayProxyResponse{}, ErrNameNotProvided
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       "Hello " + request.Body,
+		StatusCode: 200,
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(Handler)
 }
